a9acf45b439b17c57a412930e1def99eaf128c79
Kiran Patel <kiranpatel@Kirans-MacBook-Pro.local>
2014-11-07 00:21:52 +0000

    added prevention of sql injection to add form

--- add_member_post.php
+++ add_member_post.php
@@ -5,12 +5,25 @@
 	$con = mysqli_connect($host, $username, $password, $db);
 
 	$fName = $_POST['fName']; 
+	$fName = mysql_real_escape_string($fName);
+
 	$lName = $_POST['lName']; 
+	$lName = mysql_real_escape_string($lName);
+
 	$sex = $_POST['sex']; 
+	$sex = mysql_real_escape_string($sex);
+
 	$YOB = $_POST['year']; 
+	$YOB = mysql_real_escape_string($YOB);
+
 	$POB = $_POST['place']; 
-	$spouse = $_POST['Spouse']; 
+	$POB = mysql_real_escape_string($POB);
+
+	$spouse = $_POST['Spouse'];
+	$spouse = mysql_real_escape_string($spouse);
+
 	$parentID = $_POST['parentID']; 
+	$parentID = mysql_real_escape_string($parentID);
 
 	$query = "INSERT INTO relation(fName, lName, sex, YearOfBirth, PlaceOfBirth, Spouse, parentID) VALUES ('$fName', '$lName', '$sex', '$YOB', '$POB', '$spouse', '$parentID')"; 
 
